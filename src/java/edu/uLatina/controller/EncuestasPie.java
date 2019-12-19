7ff489d224f76498d3358c8f2d47ec90b06ba86b
Andres <43399076+cotoa412@users.noreply.github.com>
2019-12-19 14:01:54 -0600

    Pie Chart con ID!

--- src/java/edu/uLatina/controller/EncuestasPie.java
+++ src/java/edu/uLatina/controller/EncuestasPie.java
@@ -10,11 +10,16 @@ import com.componentes.entidades.Encuesta;
 import com.componentes.entidades.Formulario;
 import com.componentes.entidades.Item;
 import com.componentes.entidades.Seccion;
+import com.componentes.entidades.Usuario;
+import edu.uLatina.model.OpcionTexto;
+import edu.uLatina.model.SeleccionMultiple;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import org.primefaces.model.chart.PieChartModel;
 
 /**
@@ -28,6 +33,19 @@ public class EncuestasPie {
     private EncuestaController eCon = new EncuestaController();
     private Encuesta encuesta = new Encuesta();
     private PieChartModel model = new PieChartModel();
+    
+    private List<Seccion> listaSecciones = new ArrayList<>();
+    private String link = "";
+    private String tempEmail = "";
+
+    private int formId;
+    private Formulario form = null;
+    private List<Seccion> secciones;
+    private List<Item> items;
+    private Usuario u;
+    private List<SeleccionMultiple> listaSeccionSeleccionMultiple = new ArrayList<>();
+    private List<OpcionTexto> listaSeccionOpcionTexto = new ArrayList<>();
+
 
     public ArrayList<PieChartModel> conseguirRespuesta() {
         
@@ -107,7 +125,119 @@ public class EncuestasPie {
         }
         return Modelos;
     }
+    
+    public void validateId(int id) {
+        this.asignarId(id);
+        try {
 
+            if (id != 0) {
+                this.listaSeccionOpcionTexto.clear();
+                this.listaSeccionSeleccionMultiple.clear();
+
+                this.conseguirRespuesta(id);
+                HttpServletRequest request = (HttpServletRequest) FacesContext
+                        .getCurrentInstance().getExternalContext().getRequest();
+                FacesContext
+                        .getCurrentInstance()
+                        .getExternalContext()
+                        .redirect(
+                                request.getContextPath()
+                                + "/faces/pieTest.xhtml?faces-redirect=true");
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+     public void asignarId(int id) {
+
+        this.formId = id;
+
+    }
+
+    public List<Seccion> getListaSecciones() {
+        return listaSecciones;
+    }
+
+    public void setListaSecciones(List<Seccion> listaSecciones) {
+        this.listaSecciones = listaSecciones;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTempEmail() {
+        return tempEmail;
+    }
+
+    public void setTempEmail(String tempEmail) {
+        this.tempEmail = tempEmail;
+    }
+
+    public int getFormId() {
+        return formId;
+    }
+
+    public void setFormId(int formId) {
+        this.formId = formId;
+    }
+
+    public Formulario getForm() {
+        return form;
+    }
+
+    public void setForm(Formulario form) {
+        this.form = form;
+    }
+
+    public List<Seccion> getSecciones() {
+        return secciones;
+    }
+
+    public void setSecciones(List<Seccion> secciones) {
+        this.secciones = secciones;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Usuario getU() {
+        return u;
+    }
+
+    public void setU(Usuario u) {
+        this.u = u;
+    }
+
+    public List<SeleccionMultiple> getListaSeccionSeleccionMultiple() {
+        return listaSeccionSeleccionMultiple;
+    }
+
+    public void setListaSeccionSeleccionMultiple(List<SeleccionMultiple> listaSeccionSeleccionMultiple) {
+        this.listaSeccionSeleccionMultiple = listaSeccionSeleccionMultiple;
+    }
+
+    public List<OpcionTexto> getListaSeccionOpcionTexto() {
+        return listaSeccionOpcionTexto;
+    }
+
+    public void setListaSeccionOpcionTexto(List<OpcionTexto> listaSeccionOpcionTexto) {
+        this.listaSeccionOpcionTexto = listaSeccionOpcionTexto;
+    }
+
+     
 
     public EncuestaController geteCon() {
         return eCon;
